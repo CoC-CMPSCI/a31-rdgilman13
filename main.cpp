32b6a682eb49126f2eec24190ee19eb6f831382a
Rollin Gilman <rdgilman@my.canyons.edu>
2026-03-14 09:49:50 -0700

    Complete Assignment 31

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
 {
-  const double ClassACost = 15.00;
-  const double ClassBCost = 12.00;
-  const double ClassCCost = 9.00;
+    const double ClassACost = 15.00;
+    const double ClassBCost = 12.00;
+    const double ClassCCost = 9.00;
 
-  // TODO: Declare integer variables for tickets sold for each class
+    int ticketsA, ticketsB, ticketsC;
+    double totalA, totalB, totalC, total;
 
-  // TODO: Declare double variables for total sales per class and grand total
+    cout << "How many tickets were sold for Class A" << endl;
+    cin >> ticketsA;
 
-  // TODO: Prompt user and read number of Class A tickets sold
+    cout << "How many tickets were sold for Class B" << endl;
+    cin >> ticketsB;
 
-  // TODO: Prompt user and read number of Class B tickets sold
+    cout << "How many tickets were sold for Class C" << endl;
+    cin >> ticketsC;
 
-  // TODO: Prompt user and read number of Class C tickets sold
+    totalA = ticketsA * ClassACost;
+    totalB = ticketsB * ClassBCost;
+    totalC = ticketsC * ClassCCost;
+    total = totalA + totalB + totalC;
 
-  // TODO: Calculate sales for each class
+    cout << setprecision(2) << fixed;
 
-  // TODO: Calculate total sales
+    cout << "Tickets sold for Class A " << ticketsA
+         << " Sales Price for A " << totalA << endl;
 
-  cout << setprecision(2) << fixed;
-  cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A   " << totalA << endl;
-  cout << "Tickets sold for Class B " << ticketsB << " Sales Price for B   " << totalB << endl;
-  cout << "Tickets sold for Class C " << ticketsC << " Sales Price for C   " << totalC << endl;
-  cout << "Total Sales                              " << total << endl;
+    cout << "Tickets sold for Class B " << ticketsB
+         << " Sales Price for B " << totalB << endl;
+
+    cout << "Tickets sold for Class C " << ticketsC
+         << " Sales Price for C " << totalC << endl;
+
+    cout << "Total Sales " << total << endl;
+
+    return 0;
 }
